b1e797a8c648073a0666b86aff654cc861ccfca3
varun1524 <varun3273@gmail.com>
2017-11-14 00:10:04 -0800

    Implementation of Sending data to backend using producer and and receiving data from back-end using consumer added

--- kayak_react/src/api/API.js
+++ kayak_react/src/api/API.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:3001'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:3001';
 
 const headers = {
     'Accept': 'application/json'
--- kayak_react/src/index.js
+++ kayak_react/src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <div className="wrapper container">
+        <App/>
+    </div>, document.getElementById('root'));
