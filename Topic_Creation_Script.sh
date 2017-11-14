5cf0192faabdb0b84a49c3cc6577ee384fc5c7ab
varun1524 <varun3273@gmail.com>
2017-11-14 01:33:50 -0800

    topics added in script

--- Topic_Creation_Script.sh
+++ Topic_Creation_Script.sh
@@ -1,6 +1,8 @@
 cd "/Users/varunshah/opt/zookeeper"
 bin/zkServer.sh start
 cd "/Users/varunshah/opt/kafka"
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic upload_topic
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic login_topic
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic signup_topic
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic response_topic
 bin/kafka-topics.sh --list --zookeeper localhost:21
 echo "Topics Created Successfully"
