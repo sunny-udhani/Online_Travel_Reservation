ff2e25f037f58f2e00cdfda89893cd7ce45b9b5d
varun1524 <varun3273@gmail.com>
2017-11-14 02:25:43 -0800

    login and signup added

--- kayak_kafka_backend/services/login.js
+++ kayak_kafka_backend/services/login.js
@@ -1,64 +1,45 @@
-var mongo = require("./mongo");
-var mongoURL = "mongodb://localhost:27017/dropbox";
-var act = require("./activity");
+// var mongo = require("./mongo");
+// var mongoURL = "mongodb://localhost:27017/dropbox";
+let mysql = require('../mysql/mysql');
 var bcrypt = require("bcrypt");
 
-handle_request = ((data, done)=> {
-    // passport.use('login', new LocalStrategy(function(username, password, done) {
-    let res = {};
+handle_request = ((data, callback)=> {
+    let response = {
+        status : 400
+    };
     try {
         console.log("In Login");
-        mongo.connect(mongoURL, function(){
-            console.log('Connected to mongo at: ' + mongoURL);
-            var coll = mongo.collection('users');
+        let sqlQuery = "select password from users where username = '"+ data.username +"'";
+        mysql.fetchData(function (err, result) {
+            if(err){
+                console.log(err);
+            }
+            else {
 
-            coll.findOne({username: data.username}, function(err, result){
                 console.log(result);
-                if(err){
-                    console.log(err);
-                    throw err;
-                }
-                if(result!==null && result!==undefined) {
-                    if (bcrypt.compareSync(data.password, result.hashpassword)) {
-                        act.insertIntoActivity(function (err, activityInserted) {
-                            if (err) {
-                                console.log(err);
-                                res.status = 301;
-                                res.message = "Login Successful. Failed to add user activity";
-                                done(err, res);
-                            }
-                            console.log(activityInserted);
-                            if (activityInserted) {
-                                res.status = 201;
-                                res.username = data.username;
-                                res.message = "Login Successful. Login Activity Added";
-                                done(err, res);
-                            }
-                            else {
-                                res.status = 301;
-                                res.message = "Login Successful. Failed to add Activity";
-                                done(err, res);
-                            }
-                        }, data.username, "login");
+                if(result.rows===1){
+                    if(bcrypt.compareSync(data.password, result.password)){
+                        response.status = 200;
+                        response.message = "Login Credentials are correct";
                     }
                     else {
-                        res.status = 301;
-                        res.message = "Incorrect Password";
-                        done(err, res);
+                        response.message = "Password is incorrect. Please try again";
+                        response.status = 401;
                     }
+                    callback(null, response);
                 }
                 else {
-                    res.status = 301;
-                    res.message = "Username does not exist. Please signup";
-                    done(err, res);
+                    response.status = 400;
+                    response.message = "Username does not exist. Please sign up";
+                    callback(null, response);
                 }
-            });
-        });
+            }
+        }, sqlQuery);
     }
     catch (e){
-        done(e,{});
+        console.log(e);
+        callback(e, response);
     }
-    // }));
 });
 
 exports.handle_request = handle_request;
--- kayak_kafka_backend/services/signup.js
+++ kayak_kafka_backend/services/signup.js
@@ -1,121 +1,53 @@
-var act = require('./activity');
-var bcrypt = require('bcrypt');
-var UserProfile = require('../models/userprofile');
-let mongo = require('./mongo');
-let mongoURL = "mongodb://localhost:27017/dropbox";
+let act = require('./activity');
+let bcrypt = require('bcrypt');
+let mysql = require('../mysql/mysql');
+// var UserProfile = require('../models/userprofile');
+// let mongo = require('./mongo');
+// let mongoURL = "mongodb://localhost:27017/dropbox";
 
 handle_request = ((data, callback) => {
-    let err=null;
-    let response = {};
+    let response = {
+        status : 400
+    };
     try {
-        console.log(data);
-        var salt = bcrypt.genSaltSync(10);
 
-        let user = {
-            _id  : data.username,
-            firstname : data.firstname,
-            lastname : data.lastname,
-            username : data.username,
-            hashpassword : bcrypt.hashSync(data.password, salt)
-        };
+        let salt = bcrypt.genSaltSync(10);
 
-        console.log(user);
-
-        mongo.connect(mongoURL, function () {
-            let usercollection = mongo.collection('users');
-            usercollection.findOne({username: data.username}, function (err, result) {
+        let password = bcrypt.hashSync(data.password, salt);
+        let sqlQuery = "insert into users (username, password) values ('"+data.username+"','"+password+"');";
+        console.log(sqlQuery);
+        mysql.fetchData(function (err, result) {
+            if(err){
+                console.log(err);
+            }
+            else {
                 console.log(result);
-                if (err) {
-                    console.log("error:");
-                    console.log(err);
-                    throw err;
-                }
-                if (result) {
-                    console.log("User Exists");
-                    console.log(result._id);
-                    response.status = 301;
-                    response.message = "Already Exist";
-                    callback(err, response);
+                if(result.rows===1){
+                    response.status = 401;
+                    response.message = "User Already Exists";
+                    callback(null, response);
                 }
                 else {
-                    usercollection.insertOne(user, function (err, result1) {
-                        console.log("result");
-                        console.log(result1);
-
-                        if (err) {
+                    mysql.insertData(function (err, result) {
+                        if(err){
                             console.log(err);
-                            throw err;
-                        }
-
-                        if (result1.insertedCount===1) {
-                            console.log("Sign up successful");
-                            let userprofilecollection = mongo.collection("userprofiles");
-
-                            let userprofile = {
-                                _id: data.username,
-                                overview: "",
-                                work: "",
-                                education: "",
-                                contactinfo: "",
-                                lifeevents: "",
-                                music: false,
-                                sports: false,
-                                reading: false,
-                            };
-
-                            userprofilecollection.insertOne(userprofile, function (err, result2) {
-                                console.log(result2);
-                                if (err) {
-                                    console.log(err);
-                                    throw "Error while adding data into userprofile table";
-                                }
-                                if(result2.insertedCount===1){
-                                    act.insertIntoActivity(function (err, activityInserted) {
-                                        if (err) {
-                                            console.log(err);
-                                            response.status = 301;
-                                            response.message = "Signup Successful. Failed to add user activity";
-                                            callback(err, response);
-                                        }
-                                        console.log(activityInserted);
-                                        if (activityInserted) {
-                                            response.status = 201;
-                                            response.username = data.username;
-                                            response.message = "Signup Successful";
-                                            callback(err, response);
-                                        }
-                                        else {
-                                            usercollection.deleteOne({_id: data.username}, function (err, result3) {
-                                                console.log(result3);
-                                                userprofilecollection.deleteOne({_id: data.username}, function (err, result4) {
-                                                    console.log(result4);
-                                                    //delete directory here
-                                                    response.status = 401;
-                                                    response.message = "Signup Failed";
-                                                    callback(err, response);
-                                                })
-                                            })
-
-                                        }
-                                    }, data.username, "signup");
-                                }
-                                else {
-                                    usercollection.deleteOne({_id: data.username}, function (err, result3) {
-                                        response.status = 401;
-                                        response.message = "Signup Failed";
-                                        callback(err, response);
-                                    })
-                                }
-                            });
                         }
                         else {
-                            response.status = 401;
-                            response.message = "Signup Failed";
-                            callback(err, response);
+                            console.log(result);
+                            if(result.affectedRows===1){
+                                response.status = 200;
+                                response.message = "Signup Successful";
+                                callback(null, response);
+                            }
+                            else {
+                                response.status = 400;
+                                response.message = "Failed to Signup";
+                                callback(null, response);
+                            }
                         }
                     });
                 }
-            });
+            }
         });
     }
     catch (e){
