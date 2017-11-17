8bfc30e0f2de494670c8d5b64559d61a54c10df8
Pritam Meher <pritamkachardas.meher@sjsu.edu>
2017-11-16 23:17:35 -0800

    Popup form

---
+++ Working Directory/Modal/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home'
+class App extends Component {
+  render() {
+    return (
+     <div>
+         <Home/>
+     </div>
+    );
+  }
+}
+
+export default App;
