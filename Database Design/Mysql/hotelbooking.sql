bb2d91e6398f56fd28efbed9f3d5f42621c4b5a1
Vishweshkumar Patel <15657517+vishwesh07@users.noreply.github.com>
2017-11-23 15:04:50 -0800

    Update hotelbooking.sql

--- Database Design/Mysql/hotelbooking.sql
+++ Database Design/Mysql/hotelbooking.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `hotelbooking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `hotelbooking` (
-  `bookingId` mediumint(10) NOT NULL,
+  `bookingId` mediumint(10) NOT NULL AUTO_INCREMENT,
   `hotelId` mediumint(10) NOT NULL,
   `fromDate` date NOT NULL,
   `toDate` date NOT NULL,
