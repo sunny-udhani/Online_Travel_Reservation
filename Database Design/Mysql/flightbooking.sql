be053f2e5d3524a42ce00522b99266a01b56f0f6
Vishweshkumar Patel <15657517+vishwesh07@users.noreply.github.com>
2017-11-23 15:04:15 -0800

    Update flightbooking.sql

--- Database Design/Mysql/flightbooking.sql
+++ Database Design/Mysql/flightbooking.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `flightbooking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `flightbooking` (
-  `bookingId` mediumint(10) NOT NULL,
+  `bookingId` mediumint(10) NOT NULL AUTO_INCREMENT,
   `flightId` mediumint(10) NOT NULL,
   `flightClass` varchar(50) NOT NULL,
   `tripType` varchar(10) NOT NULL,
