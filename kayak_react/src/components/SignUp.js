032287cdba1ab1eeca519e58bbd4fdcd26d34413
RutvikatHub <pensionwar.rutvik@gmail.com>
2017-11-27 02:11:49 -0800

    User/Admin Signup Component

--- kayak_react/src/components/SignUp.js
+++ kayak_react/src/components/SignUp.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
-import {Link} from "react-router-dom";
-import PropTypes from 'prop-types';
-// import * as API from '../api/API';
+import {Route, withRouter} from 'react-router-dom';
+import {connect} from "react-redux"
+
+import {signUpSuccess} from "../actions/index";
+import {doSignUp} from "../api/user/API_SignUp";
+
+import '../design/css/signupform.css'
 
 class SignUp extends Component {
 
     static propTypes = {
-        handleSubmitRegister: PropTypes.func.isRequired
+        //handleSubmitRegister: PropTypes.func.isRequired
     };
 
     constructor(props) {
@@ -28,58 +32,137 @@ class SignUp extends Component {
             });
     }
 
-    handleSubmitClick(event) {
-        let formData = new FormData();
-        formData = this.state;
-        this.props.handleSubmitRegister(formData);
+    handleSubmitClick() {
+        let userdata = new FormData();
+        userdata = this.state;
+
+        console.log("Formdata : " + userdata);
+
+        doSignUp(userdata)
+            .then((res) => {
+                if (res.status === 200) {
+
+                    console.log("SignUp - username : " + res.username);
+                    console.log("Signup - message is - " + res.message);
+
+                    this.props.handleSubmitRegister(userdata.username);
+                    this.props.history.push("/u");
+                }
+                if (res.status === 201) {
+
+                    console.log("SignUp - username : " + res.username);
+                    console.log("Signup - message is - " + res.message);
+
+                    this.props.handleSubmitRegister(userdata.username);
+                    this.props.history.push("/admin");
+                }
+                else if (res.status === 401) {
+                    console.log("User Already Exists");
+                }
+                else {
+                    console.log("Failed to Signup");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     };
 
 
     render() {
 
         return (
-            <div className="row">
-                <div className="col-sm-offset-4 col-md-offset-4 col-lg-offset-4 col-sm-6 col-md-6">
-                    <div className="panel panel-default">
-                        <div className="panel panel-body">
-                            <ul id="dTab" className="nav nav-tabs">
-                                <li><Link to='/login'><span className="glyphicon glyphicon-circle-arrow-right"></span>Login</Link>
-                                </li>
-                            </ul>
-                            <div id="pane1" className="tab-pane">
-                                <form className="text-justify">
-                                    <fieldset>
-                                        <div className="form-group">
-                                            <label className="control-label">UserName</label>
-                                            <input onChange={(e) => this.handleChange(e)} type="email" id="userEmail"
-                                                   required="required" name="userEmail"
-                                                   placeholder="Enter your email id" className="form-control"/>
-                                        </div>
-                                        <div className="form-group">
-                                            <label className="control-label">Password</label>
-                                            <input onChange={(e) => this.handleChange(e)} type="password"
-                                                   required="required" id="password" name="password"
-                                                   placeholder="Enter Password" className="form-control"/>
-                                        </div>
-                                        <button
-                                            className="btn btn-primary"
-                                            type="button"
-                                            onClick={() => this.handleSubmitClick()}>
-                                            Submit
-                                        </button>
-                                    </fieldset>
-                                </form>
-                            </div>
-                            <div className="col-md-10">
-                                <br/>
-                                <img src="../../public/logo.png"></img>
-                            </div>
+            <div className="signupform">
+                <div className="form-modal" >
+                    <form className="text-justify">
+                        <div>
+                            <h5>Username</h5>
+                            <input onChange={(e) => this.handleChange(e)} type="email" id="username"
+                                   required="required" name="username"
+                                   placeholder="" className="form-control input-sm"/>
                         </div>
-                    </div>
+                        <br/>
+                        <div>
+                            <h5>Password</h5>
+                            <input onChange={(e) => this.handleChange(e)} type="password"
+                                   required="required" id="password" name="password"
+                                   placeholder="" className="form-control input-sm"/>
+                        </div>
+                        <br/>
+                        <div>
+                            <h5>First Name</h5>
+                            <input onChange={(e) => this.handleChange(e)} type="text" id="firstName"
+                                   name="firstName" placeholder=""
+                                   className="form-control input-sm"/>
+                        </div>
+                        <br/>
+                        <div>
+                            <h5>Last Name</h5>
+                            <input onChange={(e) => this.handleChange(e)} type="text" id="lastName"
+                                   name="lastName" placeholder=""
+                                   className="form-control input-sm"/>
+                        </div>
+                        <br/>
+                        <div>
+                            <h5>Date of Birth</h5>
+                            <input onChange={(e) => this.handleChange(e)} type="date" id="dob"
+                                   name="dob" className="form-control input-sm"/>
+
+                        </div>
+                        <br/>
+                        <div>
+                            <h5>Gender</h5>
+                            <input onChange={(e) => this.handleChange(e)} type="radio"
+                                   id="genderChoice1"
+                                   name="gender" value="male"/>
+                            <label className=""><h6>Male</h6></label>
+                            <input onChange={(e) => this.handleChange(e)} type="radio"
+                                   id="genderChoice2"
+                                   name="gender" value="female"/>
+                            <label className=""><h6>Female</h6></label>
+                            <input onChange={(e) => this.handleChange(e)} type="radio"
+                                   id="genderChoice4"
+                                   name="gender" value="notSpecified"/>
+                            <label className=""><h6>Prefer not to say</h6></label>
+                        </div>
+                        <br/>
+                        <div>
+                            <h5>Access Indicator</h5>
+                            <input onChange={(e) => this.handleChange(e)} type="radio"
+                                   id="accessInd1"
+                                   name="accessInd" value="admin"/>
+                            <label className=""><h6>Admin</h6></label>
+                            <input onChange={(e) => this.handleChange(e)} type="radio"
+                                   id="accessInd2"
+                                   name="accessInd" value="user"/>
+                            <label className=""><h6>User</h6></label>
+                        </div>
+                        <button
+                            className="login loginmodal-submit btn-block"
+                            type="button"
+                            onClick={() => this.handleSubmitClick()}>
+                            Submit
+                        </button>
+
+                    </form>
                 </div>
             </div>
         );
     }
 }
 
-export default SignUp;
+function mapStateToProps(state) {
+    return {
+        menu: state
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        handleSubmitRegister: (username) => {
+            dispatch(signUpSuccess(username))
+        }
+    };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SignUp));
