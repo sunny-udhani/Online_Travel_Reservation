a1b98802a5477f21f50e8a27a3d48057e3fe731b
sunny-udhani <sunny.iv09@gmail.com>
2017-11-28 03:42:52 -0800

    hotel listing with redis caching

---
+++ kayak_kafka_frontend/routes/listings.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const redis = require("redis");
+const redis_client = redis.createClient();
+
+redis_client.on('connect', function () {
+    console.log('connected');
+});
+
+const router = express.Router();
+const req_topic_enums = require('../config/topic_enum').req_topic_names;
+const kafka = require('./kafka/client');
+/* GET users listing. */
+router.post('/', function (req, res, next) {
+    res.send('respond with a resource');
+    next();
+});
+
+router.post('/getHotels', function (req, res) {
+    try {
+        if (req.body.criteria) {
+
+            redis_client.hget(req.body.criteria.toString(), req.body.criteria.toString(), function (err, reply) {
+
+                if (err) {
+
+                    console.log(err);
+                    console.log("error in hget");
+                    res.status(400).send();
+
+                } else {
+                    if (reply) {
+
+                        console.log(JSON.parse(reply));
+                        console.log("reply from cache");
+                        //send data if we get from cache - redis
+                        res.status(200).send(JSON.parse(reply));
+
+                    } else {
+
+                        //kafka request - no data found from cache
+                        kafka.make_request(req_topic_enums.HOTEL_LISTING, req.body, function (err, results) {
+
+                            console.log('in result');
+
+                            console.log(results.data);
+
+                            if (err) {
+                                console.log(err);
+                                throw err;
+                            }
+
+                            else {
+                                if (results.status === 200) {
+
+                                    redis_client.hset(req.body.criteria.toString(), req.body.criteria.toString(), JSON.stringify(results.data), function (err, reply) {
+
+                                        if (err) {
+
+                                            console.log(err);
+                                            console.log("error on hset");
+
+                                            redis_client.hdel(req.body.criteria.toString(), req.body.criteria.toString(), function (err, reply) {
+
+                                                console.log(err);
+                                                console.log("error on delete");
+                                                console.log(reply);
+                                                console.log("reply for delete");
+
+                                            })
+                                        }
+
+                                        else {
+                                            console.log("successfully set cache entry");
+                                            redis_client.expire(req.body.criteria.toString(), 30);
+                                            res.status(results.status).send(results.data);
+                                        }
+
+                                    });
+
+                                }
+                                else if (results.status === 400) {
+                                    res.status(results.status).send();
+                                }
+                            }
+                        });
+                    }
+                }
+            })
+        }
+
+        // kafka.make_request(req_topic_enums.HOTEL_LISTING, req.body, function (err, results) {
+        //     console.log('in result');
+        //     console.log(results.data);
+        //     if (err) {
+        //         console.log(err);
+        //         throw err;
+        //     }
+        //     else {
+        //         if (results.status === 200) {
+        //             redis_client.hset(req.body.criteria.toString(), req.body.criteria.toString(), JSON.stringify(results.data), function (err, reply) {
+        //                 if (err) {
+        //
+        //                     console.log(err);
+        //                     console.log("error on hset");
+        //                     redis_client.del(req.body.criteria.toString(), req.body.criteria.toString(), function (err, reply) {
+        //                         console.log(err);
+        //                         console.log("error on delete");
+        //                         console.log(reply);
+        //                         console.log("reply for delete");
+        //                     })
+        //                 }
+        //                 else {
+        //                     res.status(results.status).send(results.data);
+        //                 }
+        //             });
+        //         }
+        //         else if (results.status === 400) {
+        //             res.status(results.status).send();
+        //         }
+        //     }
+        // });
+    }
+    catch (e) {
+        console.log(e);
+        res.status(400).json({message: "search Failed"});
+    }
+});
+
+
+module.exports = router;
