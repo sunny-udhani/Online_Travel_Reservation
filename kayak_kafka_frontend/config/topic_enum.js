89270c265a711d692f82ee3ad176166b6ccb1209
sunny-udhani <sunny.iv09@gmail.com>
2017-11-28 03:40:16 -0800

    topic-enum config

---
+++ kayak_kafka_frontend/config/topic_enum.js
@@ -0,0 +1,15 @@
+exports.req_topic_names = {
+REGISTER: 'register_request',
+    LOGIN: 'login_request',
+    LOGOUT: 'logout_request',
+    HOTEL_LISTING: 'hotel_listing_request',
+
+};
+//
+// exports.res_topic_names = {
+//     REGISTER: 'register_response',
+//     LOGIN: 'login_response',
+//     LOGOUT: 'logout_response',
+//     HOTEL_LISTING: 'hotel_listing_response',
+//
+// };
