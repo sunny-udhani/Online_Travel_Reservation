a4a8c45bdeb0259883c9686fe02815022eede7a0
sunny-udhani <sunny.iv09@gmail.com>
2017-11-28 04:01:12 -0800

    merge and listing commit

--- kayak_react/src/actions/index.js
+++ kayak_react/src/actions/index.js
@@ -8,9 +8,24 @@ export function login_success(email, message) {
     }
 }
 
-export function signUp(userDet) {
+export function signUpSuccess(username) {
     return {
         type: actionTypes.SIGN_UP,
-        userDet
+        username
+    }
+}
+
+
+export function filter_change(filterInd) {
+    return {
+        type: actionTypes.FILTER_PRICE_CHANGE,
+        filterInd
+    }
+}
+
+export function hotelList_Success(hotelList) {
+    return {
+        type: actionTypes.HOTEL_LISTING_SUCCESS,
+        hotelList
     }
 }
--- kayak_react/src/components/Kayak.js
+++ kayak_react/src/components/Kayak.js
@@ -1,54 +1,175 @@
 import React, {Component} from 'react';
-import { Route, withRouter, Switch } from 'react-router-dom';
+import {Route, withRouter, Switch, Link} from 'react-router-dom';
 import AdminHome from "./admin/AdminHome";
-import UserHome from "./user/UserHome";
+import UserPaymentPage from "./user/UserPaymentPage";
+
 import Login from "./Login";
-import HostHome from "./host/HostHome";
+import SignUp from "./SignUp";
+import Home from "./user/Home";
+
+import '../design/css/home.css'
+
+import * as API from "../api/API";
+
+import UserHome from "./user/UserHome";
+
+import "../css/bootstrap.min.css"
+import "../css/font-awesome.min.css"
+import "../css/style.css"
+import "../css/jquery-ui.min.css"
+import "../css/jquery-ui.structure.min.css"
+import Listing from "./user/Listing";
 
 class Kayak extends Component {
-
 
     handleSubmit = (userdata) => {
     };
 
+    handlePageChange = ((page) => {
+        this.props.history.push(page);
+    });
+
+    componentWillMount() {
+        // this.showLoginOption();
+    }
+
+    handleLogout = (() => {
+        API.doLogout().then((response) => {
+            console.log(response.status);
+            if (response.status === 201) {
+
+            }
+        });
+    });
+
+    showLoginOption = ((item) => {
+        console.log(item);
+        console.log(this.props.admi);
+        API.validateSession().then((response) => {
+            if (response.status === 200) {
+                let username;
+                response.json().then((data) => {
+                    username = data.username;
+                });
+                return (
+                    <div>
+                        User Email : {this.username}
+                    </div>
+                )
+            }
+            // else if(response.status===201){
+            //     let username;
+            //     response.json().then((data)=>{
+            //         username = data.username;
+            //     });
+            //     return(
+            //         <div>
+            //             Admin Email : {this.username}
+            //         </div>
+            //     )
+            // }
+            else {
+                return (
+                    <div>
+                        <Link to='/login'><span className="glyphicon glyphicon-circle-arrow-right"></span>Login</Link>
+                        <Link to='/signup'><span className="glyphicon glyphicon-circle-arrow-right"></span>Sign
+                            Up</Link>
+                    </div>
+                )
+            }
+        });
+
+
+    });
+
     render() {
         return (
             <div>
-                <div className="container-fluid">
-                    <hr/>
-                        //Header
-                    <hr/>
-                    {/*<Link to='/admin'><span className="glyphicon glyphicon-circle-arrow-right"></span>Admin</Link>*/}
+                <div className="container">
+                    <header className="color-1 hovered menu-3">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="nav">
+                                        <nav className="menu">
+                                            <ul>
+                                                <li className="type-1 active">
+                                                    <a href="#">home<span className="fa fa-angle-down"></span></a>
+
+                                                </li>
+                                                <li className="type-1"><a href="#">Hotels
+                                                    <span
+                                                        className="fa fa-angle-down"></span></a>
+
+                                                </li>
+                                                <li className="type-1"><a href="#">Flights
+                                                    <span
+                                                        className="fa fa-angle-down"></span></a>
+
+                                                </li>
+                                                <li className="type-1"><a href="#">Cars
+                                                    <span
+                                                        className="fa fa-angle-down"></span></a>
+
+                                                </li>
+                                                <li className="type-1"><a href="#">Cruises
+                                                    <span
+                                                        className="fa fa-angle-down"></span></a>
+
+                                                </li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+                </div>
+
+                <div className="container">
+                    {/*<Link to='/admin'><span classNameName="glyphicon glyphicon-circle-arrow-right"></span>Admin</Link>*/}
                     <Switch>
-                        <Route exact path="/" render={() =>
-                            <UserHome/>
+                        <Route exact path="/" render={() => {
+                            this.props.history.push("/u")
+                        }
+                        }/>
+
+                        <Route path="/payment" render={() =>
+                            <UserPaymentPage/>
                         }/>
 
                         <Route path="/u" render={() =>
-                            <UserHome/>
+                            <UserHome
+                                handlePageChange={this.handlePageChange}
+                            />
+                        }/>
+
+                        <Route path="/listing" render={() =>
+                            <Listing/>
                         }/>
 
                         <Route path="/signup" render={() =>
-                            <Login handleSubmit={this.handleSubmit} invalidateUserSession={this.invalidateUserSession}/>
+                            <SignUp
+                                handleSubmit={this.handleSubmit}
+                                invalidateUserSession={this.invalidateUserSession}/>
                         }/>
 
                         <Route path="/login" render={() =>
-                            <Login handleSubmit={this.handleSubmit} invalidateUserSession={this.invalidateUserSession}/>
+                            <Login
+                                handleSubmit={this.handleSubmit}
+                                invalidateUserSession={this.invalidateUserSession}
+                                handlePageChange={this.handlePageChange}
+                            />
                         }/>
 
                         <Route path="/admin" render={() => (
-                            <AdminHome/>
+                            <AdminHome
+                                handleLogout={this.handleLogout}
+                            />
                         )}/>
 
-                        <Route path="/host" render={() => (
-                            <HostHome/>
-                        )}/>
                     </Switch>
                 </div>
-                <br/>
-                <hr/>
-                    Footer
-                <hr/>
             </div>
         )
     }
