6314df60ea4afa3aff5b9ced79e3775e375e481a
varun1524 <varun3273@gmail.com>
2017-11-30 02:55:05 -0800

    Route to CarPage added

--- kayak_react/src/components/admin/AdminHome.js
+++ kayak_react/src/components/admin/AdminHome.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { Route, withRouter, Switch, Link } from 'react-router-dom';
 import HotelPage from "./hotel/HotelPage";
 import FlightPage from "./flight/FlightPage";
-import CarPage from "./CarPage";
+import CarPage from "./car/CarPage";
 import AdminProfile from "./AdminProfile";
 
 class AdminHome extends Component {
@@ -16,57 +16,58 @@ class AdminHome extends Component {
             <div className="list-wrapper">
                 <div className="container">
                     <div className="row">
-            {/*<div className="container-fluid">*/}
-                //Admin<br/><hr/>
-                <Link to='/admin/hotel'><span className="glyphicon glyphicon-circle-arrow-right"></span>Hotel</Link>
-                <Link to='/admin/flight'><span className="glyphicon glyphicon-circle-arrow-right"></span>Flight</Link>
-                <Link to='/admin/car'><span className="glyphicon glyphicon-circle-arrow-right"></span>Car</Link>
-                <Link to='/admin/profile'><span className="glyphicon glyphicon-circle-arrow-right"></span>Profile</Link>
-                <button className="btn btn-link" onClick={(()=>{this.props.handleLogout()})}>Logout</button>
-                <Switch>
-                    <Route exact path="/admin" render={() => {
-                        return (
-                            <div>
-                                Admin Home
-                            </div>
-                        )
-                    }}/>
+                        {/*<div className="container-fluid">*/}
+                        //Admin<br/><hr/>
+                        <Link to='/admin/hotel'><span className="glyphicon glyphicon-circle-arrow-right"></span>Hotel</Link>
+                        <Link to='/admin/flight'><span className="glyphicon glyphicon-circle-arrow-right"></span>Flight</Link>
+                        <Link to='/admin/car'><span className="glyphicon glyphicon-circle-arrow-right"></span>Car</Link>
+                        <Link to='/admin/profile'><span className="glyphicon glyphicon-circle-arrow-right"></span>Profile</Link>
+                        <button className="btn btn-link" onClick={(()=>{this.props.handleLogout()})}>Logout</button>
+                        <Switch>
+                            <Route exact path="/admin" render={() => {
+                                return (
+                                    <div>
+                                        Admin Home
+                                    </div>
+                                )
+                            }}/>
 
-                    <Route path="/admin/hotel" render={() => (
-                        <HotelPage
-                            validateUserSession={this.validateUserSession}
-                            handleLogout={this.handleLogout}
-                            handlePageChange={this.props.handlePageChange}
-                            // username={this.state.username}
-                        />
-                    )}/>
+                            <Route path="/admin/hotel" render={() => (
+                                <HotelPage
+                                    // validateUserSession={this.validateUserSession}
+                                    handleLogout={this.handleLogout}
+                                    handlePageChange={this.props.handlePageChange}
+                                    // username={this.state.username}
+                                />
+                            )}/>
 
-                    <Route path="/admin/flight" render={() => (
-                        <FlightPage
-                            validateUserSession={this.validateUserSession}
-                            handleLogout={this.handleLogout}
-                            handlePageChange = {this.props.handlePageChange}
-                            // username={this.state.username}
-                        />
-                    )}/>
+                            <Route path="/admin/flight" render={() => (
+                                <FlightPage
+                                    // validateUserSession={this.validateUserSession}
+                                    handleLogout={this.handleLogout}
+                                    handlePageChange = {this.props.handlePageChange}
+                                    // username={this.state.username}
+                                />
+                            )}/>
 
-                    <Route path="/admin/car" render={() => (
-                        <CarPage
-                            validateUserSession={this.validateUserSession}
-                            handleLogout={this.handleLogout}
-                            // username={this.state.username}
-                        />
-                    )}/>
+                            <Route path="/admin/car" render={() => (
+                                <CarPage
+                                    // validateUserSession={this.props.validateUserSession}
+                                    handleLogout={this.handleLogout}
+                                    handlePageChange = {this.props.handlePageChange}
+                                    // username={this.state.username}
+                                />
+                            )}/>
 
-                    <Route path="/admin/profile" render={() =>
-                        <AdminProfile
-                            // validateUserSession={this.validateUserSession}
-                            // handleLogout={this.handleLogout}
-                            // username={this.state.username}
-                        />
-                    }/>
-                </Switch>
-            </div>
+                            <Route path="/admin/profile" render={() =>
+                                <AdminProfile
+                                    // validateUserSession={this.validateUserSession}
+                                    // handleLogout={this.handleLogout}
+                                    // username={this.state.username}
+                                />
+                            }/>
+                        </Switch>
+                    </div>
                 </div>
             </div>
         );
