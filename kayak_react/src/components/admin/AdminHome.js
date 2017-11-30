7558f44ca031fbae08eb499fb09075930f676f5c
varun1524 <varun3273@gmail.com>
2017-11-29 17:01:40 -0800

    Route to HotelPage added

--- kayak_react/src/components/admin/AdminHome.js
+++ kayak_react/src/components/admin/AdminHome.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Route, withRouter, Switch, Link } from 'react-router-dom';
-import HotelPage from "./HotelPage";
+import HotelPage from "./hotel/HotelPage";
 import FlightPage from "./FlightPage";
 import CarPage from "./CarPage";
 import AdminProfile from "./AdminProfile";
@@ -13,7 +13,10 @@ class AdminHome extends Component {
 
     render() {
         return (
-            <div className="container-fluid">
+            <div className="list-wrapper">
+                <div className="container">
+                    <div className="row">
+            {/*<div className="container-fluid">*/}
                 //Admin<br/><hr/>
                 <Link to='/admin/hotel'><span className="glyphicon glyphicon-circle-arrow-right"></span>Hotel</Link>
                 <Link to='/admin/flight'><span className="glyphicon glyphicon-circle-arrow-right"></span>Flight</Link>
@@ -21,7 +24,6 @@ class AdminHome extends Component {
                 <Link to='/admin/profile'><span className="glyphicon glyphicon-circle-arrow-right"></span>Profile</Link>
                 <button className="btn btn-link" onClick={(()=>{this.props.handleLogout()})}>Logout</button>
                 <Switch>
-
                     <Route exact path="/admin" render={() => {
                         return (
                             <div>
@@ -34,6 +36,7 @@ class AdminHome extends Component {
                         <HotelPage
                             validateUserSession={this.validateUserSession}
                             handleLogout={this.handleLogout}
+                            handlePageChange={this.props.handlePageChange}
                             // username={this.state.username}
                         />
                     )}/>
@@ -62,6 +65,8 @@ class AdminHome extends Component {
                         />
                     }/>
                 </Switch>
+            </div>
+                </div>
             </div>
         );
     }
