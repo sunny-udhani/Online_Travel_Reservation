f8e29d09ee278d03cf91c5ea3e6b49a893e71586
Vishwesh <Vishwesh.carrier@outlook.com>
2017-11-30 13:32:09 -0800

    Added route to dashboard

--- kayak_kafka_frontend/routes/admin.js
+++ kayak_kafka_frontend/routes/admin.js
@@ -226,6 +226,44 @@ router.post('/modifyHotel', function(req, res, next) {
     // res.send('respond with a resource');
 });
 
+router.post('/dashboard', function(req, res, next) {
+    console.log(req.session.username);
+    let username = req.session.username;
+    //Uncomment when Session issue is resolved
+    // if(req.session.username!==null && req.session.username!==undefined){
+    console.log(req.body);
+
+    kafka.make_request(req_topic_enums.GET_CHART_DATA, req.body, function(err,results){
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            console.log(results);
+            if(results.status === 200){
+                console.log("Local username: "+ username);
+                res.status(results.status).send(results.data);
+            }
+            else if(results.status === 300){
+                console.log("Local username: "+ username);
+                res.status(results.status).end();
+            }
+            else if(results.status === 400){
+                console.log("Local username: "+ username);
+                res.status(results.status).end();
+            }
+            else {
+                res.status(results.status).end();
+            }
+        }
+    });
+    // }
+    // else {
+    //
+    // }
+
+    // res.send('respond with a resource');
+});
 
 
 module.exports = router;
