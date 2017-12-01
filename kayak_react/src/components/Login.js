8c8fbfcf2a3d6cee7664fce121b5a90deea13fd7
Vishwesh <Vishwesh.carrier@outlook.com>
2017-11-30 16:29:10 -0800

    Added logic for logging clicksPerPage

--- kayak_react/src/components/Login.js
+++ kayak_react/src/components/Login.js
@@ -6,6 +6,7 @@ import {login_success} from "../actions";
 import {doSignIn} from "../api/user/API_SignIn";
 
 import '../design/css/signinform.css'
+import * as LogAPI from "../api/user/API_Logging";
 
 class Login extends Component {
 
@@ -14,8 +15,18 @@ class Login extends Component {
         password: ""
     };
 
-    componentWillMount() {
-
+    componentWillMount(){
+        let click = {
+            // userId: "anonymous",
+            pageName: "SignIn",
+            timeStamp: new Date().toLocaleTimeString()
+        };
+        console.log(click);
+        LogAPI.logClicksPerPage(click)
+            .then(res => {
+                console.log(`Logged ${click} status: ${res.status}`);
+            })
+            .catch(err => console.log(err));
     }
 
     handleSignIn(userdata) {
