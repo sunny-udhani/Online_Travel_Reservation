dbd6f165126fb0669ebabd5e3ba91c8adc743968
Pritam Meher <pritamkachardas.meher@sjsu.edu>
2017-12-03 20:12:21 -0800

    addUsercard

--- kayak_kafka_backend/services/addUserCard.js
+++ kayak_kafka_backend/services/addUserCard.js
@@ -6,11 +6,11 @@ handle_request = ((data, callback) => {
         status: 400
     };
     try {
-        let street = data.streetone + "," + data.streettwo;
-        let validity = data.monthexpiry + "-" + data.yearexpiry;
+        let street = data.usercard.streetone + "," + data.usercard.streettwo;
+        let validity = data.usercard.monthexpiry + "-" + data.usercard.yearexpiry;
         console.log(validity);
-        let insertCard2 = "insert into userprofile(username,street,state,zipCode) values ('" + data.username + "','" + street + "','" + data.state + "','" + data.postcode + "');";
-        let insertCard = "insert into paymentdetails(username, nameoncard, creditCardNumber,validThrough,cvv) values ('" + data.username + "','" + data.nameoncard + "','" + data.cardnumber + "','" + validity + "','"+data.cvv+"');";
+        let insertCard2 = "insert into billingaddress(username,street1,street2,postalcode,city,state,country) values ('" + data.username + "','" + data.usercard.streetone+ "','" + data.usercard.streettwo + "','" + data.usercard.postcode + "','"+data.usercard.city+"','"+data.usercard.stateuser+"','"+data.usercard.country+"');";
+        let insertCard = "insert into paymentdetails(username, nameoncard, creditCardNumber,validThrough,cvv) values ('" + data.username + "','" + data.usercard.nameoncard + "','" + data.usercard.cardnumber + "','" + validity + "','"+data.usercard.cvv+"');";
         console.log("---SQL Query " + insertCard);
 
         mysql.insertData(function (err, result) {
@@ -21,10 +21,27 @@ handle_request = ((data, callback) => {
             else {
                 // console.log(result);
                 if (result.affectedRows === 1) {
-                    response.status = 200;
-                    response.username = data.userEmail;
-                    response.message = "Added Card Successfully";
-                    callback(null, response);
+                    mysql.insertData(function (err, result) {
+                        console.log("***********************Query 2")
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            // console.log(result);
+                            if (result.affectedRows === 1) {
+
+                                response.status = 200;
+                                response.username = data.username;
+                                response.message = "Added Card Successfully";
+                                callback(null, response);
+                            }
+                            else {
+                                response.status = 400;
+                                response.message = "Failure in adding Billing Address";
+                                callback(null, response);
+                            }
+                        }
+                    }, insertCard2);
                 }
                 else {
                     response.status = 400;
