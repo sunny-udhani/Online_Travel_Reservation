89b90e8af8872ac2d85ea863118d0735ca49d1df
Pritam Meher <pritamkachardas.meher@sjsu.edu>
2017-12-03 20:15:21 -0800

    Get Hotels Booking History

---
+++ kayak_kafka_backend/services/getUserBooking_Hotels.js
@@ -0,0 +1,78 @@
+let mysql = require('../mysql/mysql');
+var mongo = require('../mongo/mongo.js');
+var mongoURL = "mongodb://kayak:kayak@cluster0-shard-00-00-j61pv.mongodb.net:27017,cluster0-shard-00-01-j61pv.mongodb.net:27017,cluster0-shard-00-02-j61pv.mongodb.net:27017/kayak?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+let async = require("async");
+let ObjectID = require("mongodb").ObjectID;
+let db = null;
+mongo.connect(mongoURL, function (db_actual) {
+    db = db_actual;
+})
+
+
+handle_request = ((data, callback) => {
+    console.log("*************************************Hotels");
+    let response = {
+        status: 400
+    };
+    let username = data.username;
+    let hotelId = '';
+    try {
+        let gethotelbookingsinfo = "select * from hotelbooking where hotelbooking.username = '" + data.username + "' ";
+
+        mysql.fetchData(function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                if (result.length !== 0) {
+                    let resulthotels = [];
+
+                    async.forEachOf(result, function (booking, index, cb) {
+                        console.log(booking);
+                        hotelId = booking.hotelId;
+
+                        db.collection('hotels').findOne({_id: new ObjectID(hotelId)}, function (err, hotelDetails) {
+                            console.log(hotelDetails);
+                            console.log(err);
+                            if (hotelDetails) {
+                                let hotel_booking = {one: {}, two: {}}
+
+                                hotel_booking.two = booking;
+                                hotel_booking.one = hotelDetails;
+                                resulthotels.push(hotel_booking);
+                                cb();
+                                // console.log("******************in hotel final result");
+                                // console.log(resulthotels)
+                            }
+                            else {
+                                cb(err);
+                                console.log("in error");
+
+                            }
+                        });
+
+
+                    }, function (err) {
+                        if (err) {
+                            callback(err)
+                        } else {
+                            console.log("flights -- ");
+
+                            console.log(resulthotels);
+                            callback(null, resulthotels);
+                        }
+                    });
+                }
+
+
+            }
+        }, gethotelbookingsinfo);
+    }
+
+    catch (e) {
+        console.log(e);
+        callback(e, null)
+    }
+});
+
+exports.handle_request = handle_request;
