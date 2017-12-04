486ba93e57832021107c39ba4f212e771030ecab
Pritam Meher <pritamkachardas.meher@sjsu.edu>
2017-12-03 20:28:25 -0800

    Preferences

--- kayak_react/src/components/user/HotelList/Preferences/Preferences.js
+++ kayak_react/src/components/user/HotelList/Preferences/Preferences.js
@@ -3,20 +3,49 @@ import './styles-user/preferences.css';
 import {Route, withRouter} from 'react-router-dom';
 import '../bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
-import {connect} from "react-redux"
+import {connect} from "react-redux";
+import {getuserprofile_user} from '../../../../api/user/API_GetUserProfile';
+import Moment from "react-moment"
+import {Link} from 'react-router-dom';
+
 // import ProfileIconEditor from './ProfileIconEditor';
 
  class Preferences extends Component{
 
-    constructor(){
+    constructor() {
         super();
- }
-    componenentWillMount(){
+        this.state = ({
+            detailed:[]
+        });
+
     }
+    componentWillMount(){
+        getuserprofile_user().then((res) => {
+
+            console.log(res);
+            console.log(this.state);
+
+            this.setState({
+                ...this.state,
+               detailed:res
+
+            });
+            console.log(this.state);
+
+        }).catch((err) => {
+
+            console.log(err);
+        })
+    }
+
+
 
     render(){
         return(
             <div className="container-fluid" >
+                {
+                    this.state.detailed.map((details,index)=>(
+
             <div className="row" style={{marginTop : "9%", backgroundColor: "#ffffff"}}>
             <div className="col-md-3-navFileUpload">
             <Navbar/>
@@ -25,49 +54,120 @@ import {connect} from "react-redux"
                 <br/><br/>
                 <div className="heading-container">
                 <h5 className="header-preferences">Preferences</h5>
-
                 </div>
                     <hr className="line"></hr>
                     <div className="email-details">
-                    <div className="email-user pull-left">
-                        <p className="user-bold">Email Login</p>
-                    </div>
-                    <div className="email-info">
-                        <p>abc@gmail.com</p>
-                    </div>
-                    </div>
-                    <div className="password-details">
-                   <div className="password-user pull-left">
-                   <p className="user-bold">Password</p>
-                   </div>
-                    <div className="password-info">
-                        <p>ddss</p>
-                    </div>
-                    </div>
-                    <div className="airport-details">
-                    <div className="airport-user pull-left">
-                    <p className="user-bold">Home-Airport</p>
-                    </div>
-                     <div className="airport-info">
-                     <p>San Francisco</p>
-                     </div>
-                    </div>
+                                <div className="email-user pull-left">
+                                    <p className="user-bold">Email Login</p>
+                                </div>
+                                <div className="email-info">
+                                <p>{this.props.email}</p>
+                        </div>
+                        </div>
 
-                    <div className="delete-details">
-                        <div className="delete-user pull-left">
-                            <button type="button" id="delete-info">Delete Account</button>
-                </div>
-            </div>
-                </div>
-            </div>
-            </div>
+
+                            <div className="password-details">
+                                <div className="password-user pull-left">
+                                    <p className="user-bold">FirstName</p>
+                                </div>
+                                <div className="password-info">
+                                    <p>{details.firstName}</p>
+                                </div>
+                            </div>
+
+                            <div className="password-details">
+                                <div className="password-user pull-left">
+                                    <p className="user-bold">Lastname</p>
+                                </div>
+                                <div className="password-info">
+                                    <p>{details.lastName}</p>
+                                </div>
+                            </div><div className="password-details">
+                                <div className="password-user pull-left">
+                                    <p className="user-bold">Gender</p>
+                                </div>
+                                <div className="password-info">
+                                    <p>{details.gender}</p>
+                                </div>
+                            </div>
+                            <div className="password-details">
+                                <div className="password-user pull-left">
+                                    <p className="user-bold">Street</p>
+                                </div>
+                                <div className="password-info">
+                                    <p>{details.street}</p>
+                                </div>
+                            </div>
+                            <div className="password-details">
+                                <div className="password-user pull-left">
+                                    <p className="user-bold">City</p>
+                                </div>
+                                <div className="password-info">
+                                    <p>{details.city}</p>
+                                </div>
+                            </div>
+
+                            <div className="password-details">
+                                <div className="password-user pull-left">
+                                    <p className="user-bold">State</p>
+                                </div>
+                                <div className="password-info">
+                                    <p>{details.state}</p>
+                                </div>
+                            </div>
+                            <div className="password-details">
+                                <div className="password-user pull-left">
+                                    <p className="user-bold">ZipCodee</p>
+                                </div>
+                                <div className="password-info">
+                                    <p>{details.zipCode}</p>
+                                </div>
+                            </div>
+                            <div className="password-details">
+                                <div className="password-user pull-left">
+                                    <p className="user-bold">Phone number</p>
+                                </div>
+                                <div className="password-info">
+                                    <p>{details.phoneNumber}</p>
+                                </div>
+                            </div>
+
+
+                            <div className="airport-details">
+                                <div className="airport-user pull-left">
+                                    <p className="user-bold">Date of Birth</p>
+                                </div>
+                                <div className="airport-info">
+                                    <p><Moment  format="YYYY/MM/DD">{details.dateofbirth}</Moment></p>
+                                </div>
+                            </div>
+
+                            <div className="delete-details">
+                                <Link classname="btn btn-danger" id="cancelcard" to = "/editdetails">Edit </Link>
+                            </div>
+
+
+                        </div>
+
+                        </div>
+                    ))
+                }
+                        </div>
+
+
+
+
+
+
+
+
         );
     }
 }
 
 function mapStateToProps(state) {
     return {
-        username: state.username
+        email: state.email
     }
 }
-export default connect(mapStateToProps)(Preferences);
+export default connect(mapStateToProps,null)(Preferences);
