6141344db8808e9da18d05009011f2927457e1f7
Vishwesh <Vishwesh.carrier@outlook.com>
2017-12-06 03:05:53 -0800

    Added log file for logging user tracing tree

---
+++ kayak_kafka_frontend/config/userTraceLog.js
@@ -0,0 +1,35 @@
+const winston = require('winston');
+const fs = require('fs');
+const logDir = 'log';
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+const tsFormat = () => (new Date()).toLocaleTimeString();
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console({
+            timestamp: tsFormat,
+            level: 'info',
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+        }),
+        new winston.transports.File({
+            filename: `${logDir}/userTraceTree.log`,
+            timestamp: tsFormat,
+            level: 'info'
+        })
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({
+            filename: `${logDir}/errors.log`,
+        })
+    ]
+});
+
+module.exports = logger;
