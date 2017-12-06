72b57b5e57a9af634d582f4d578d32e3f8e514bb
varun1524 <varun3273@gmail.com>
2017-12-06 13:34:46 -0800

    Exception handled

--- kayak_kafka_frontend/routes/logger.js
+++ kayak_kafka_frontend/routes/logger.js
@@ -3,28 +3,28 @@ var winston = require('../config/winston');
 var router = express.Router();
 
 router.post('/logClicksPerPage', function(req, res, next) {
-
-    console.log(req.session.username);
-
-    let username = req.session.username;
-
-    //Uncomment when Session issue is resolved
-    // if(req.session.username!==null && req.session.username!==undefined){
-    console.log(req.body);
-
-    if(req.session.username){
-        req.body.pageClick.userId = username ;
-    }
-
-    console.log("UserId "+req.body.pageClick.userId);
-
-    let log = {pageClick: req.body.pageClick};
-    console.log(log);
-    console.log("Log added - ");
-
-    winston.info(log);
-
     try{
+        console.log(req.session.username);
+
+        let username = req.session.username;
+
+        //Uncomment when Session issue is resolved
+        // if(req.session.username!==null && req.session.username!==undefined){
+        console.log(req.body);
+
+        if(req.session.username){
+            req.body.pageClick.userId = username ;
+        }
+
+        console.log("UserId "+req.body.pageClick.userId);
+
+        let log = {pageClick: req.body.pageClick};
+        console.log(log);
+        console.log("Log added - ");
+
+        winston.info(log);
+
+
         if(req.session.username!==null && req.session.username!==undefined){
             let nwTime = new Date().getTime();
             if(req.session.flag){
@@ -47,42 +47,42 @@ router.post('/logClicksPerPage', function(req, res, next) {
             //
             // }
         }
+
+
+        // write code for updating index as new entry is logged
+
+        res.send("Log added for "+ username+" for page - "+req.body.pageClick.pageName+" at "+req.body.pageClick.timeStamp);
+
+        // let cond = true;
+        //
+        // let temp = {};
+        //
+        // let logs = [];
+        //
+        // require('fs').readFileSync('../kayak_kafka_frontend/log/analytics.log').toString().split('\r').forEach(function (line) {
+        //     if(cond){
+        //         temp = line.split('\n')[0];
+        //         cond = false;
+        //     }
+        //     else{
+        //         temp = line.split('\n')[1];
+        //     }
+        //     if(temp === ""){
+        //         // for last line of file -- do nothing
+        //     }
+        //     else{
+        //         let a = JSON.parse(temp);
+        //         logs.push(a);
+        //     }
+        // });
+        //
+        // console.log(logs.length);
+        //
+        // console.log(logs);
     }
     catch(err){
         console.log(err);
     }
-
-    // write code for updating index as new entry is logged
-
-    res.send("Log added for "+ username+" for page - "+req.body.pageClick.pageName+" at "+req.body.pageClick.timeStamp);
-
-    // let cond = true;
-    //
-    // let temp = {};
-    //
-    // let logs = [];
-    //
-    // require('fs').readFileSync('../kayak_kafka_frontend/log/analytics.log').toString().split('\r').forEach(function (line) {
-    //     if(cond){
-    //         temp = line.split('\n')[0];
-    //         cond = false;
-    //     }
-    //     else{
-    //         temp = line.split('\n')[1];
-    //     }
-    //     if(temp === ""){
-    //         // for last line of file -- do nothing
-    //     }
-    //     else{
-    //         let a = JSON.parse(temp);
-    //         logs.push(a);
-    //     }
-    // });
-    //
-    // console.log(logs.length);
-    //
-    // console.log(logs);
-
 });
 
 // router.post('/logClicksPerProperty', function(req, res, next) {
