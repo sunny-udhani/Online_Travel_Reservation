1a7af48e522bc287ec4773d25e382ba80d782ad4
varun1524 <varun3273@gmail.com>
2017-12-31 19:56:18 -0800

    URL for new mongoDB cluster added

--- kayak_kafka_frontend/app.js
+++ kayak_kafka_frontend/app.js
@@ -14,7 +14,7 @@ let admin = require('./routes/admin');
 let listings = require('./routes/listings');
 let listingDetail = require('./routes/listingDetail');
 
-let mongoSessionURL = "mongodb://kayak:kayak@cluster0-shard-00-00-j61pv.mongodb.net:27017,cluster0-shard-00-01-j61pv.mongodb.net:27017,cluster0-shard-00-02-j61pv.mongodb.net:27017/kayak?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+let mongoSessionURL = "mongodb://kayak:kayak@kayakcluster-shard-00-00-j61pv.mongodb.net:27017,kayakcluster-shard-00-01-j61pv.mongodb.net:27017,kayakcluster-shard-00-02-j61pv.mongodb.net:27017/kayak?ssl=true&replicaSet=KayakCluster-shard-0&authSource=admin";
 let expressSessions = require("express-session");
 let mongoStore = require("connect-mongo/es5")(expressSessions);
 
