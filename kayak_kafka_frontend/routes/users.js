95d207a824dcaf0a7cdbd3cb4683ec1c3c87d7a7
varun1524 <varun3273@gmail.com>
2017-12-31 19:56:05 -0800

    URL for new mongoDB cluster added

--- kayak_kafka_frontend/routes/users.js
+++ kayak_kafka_frontend/routes/users.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 var path = require('path');
 let MongoClient = require('mongodb').MongoClient;
 // let newDate = require('mongodb').MongoClient;
-var mongoURL = "mongodb://kayak:kayak@cluster0-shard-00-00-j61pv.mongodb.net:27017,cluster0-shard-00-01-j61pv.mongodb.net:27017,cluster0-shard-00-02-j61pv.mongodb.net:27017/kayak?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+var mongoURL = "mongodb://kayak:kayak@kayakcluster-shard-00-00-j61pv.mongodb.net:27017,kayakcluster-shard-00-01-j61pv.mongodb.net:27017,kayakcluster-shard-00-02-j61pv.mongodb.net:27017/kayak?ssl=true&replicaSet=KayakCluster-shard-0&authSource=admin";
 let db = null;
 MongoClient.connect(mongoURL, function (err, _db) {
     if (err) {
